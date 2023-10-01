367a832e96d82f2bd801f47f29d7819cb25752cd
ratundo <sgop636@gmail.com>
2023-10-01 14:50:56 +0300

    added envs, nginx, blog 2 views

--- src/config/settings/dev_local.py
+++ src/config/settings/dev_local.py
@@ -1,6 +1,3 @@
-import os
-import sys
-
 from config.settings.base import *  # NOQA
 
 DEBUG = True
@@ -17,15 +14,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-# if "test" in sys.argv or "test_coverage" in sys.argv:
-#     # Disable GDAL for tests
-#     DATABASES["default"]["ENGINE"] = "django.db.backends.sqlite3"
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
